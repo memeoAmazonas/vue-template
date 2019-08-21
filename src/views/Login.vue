a06d6d2d80e4fe1c15963cef5598dbb863b6f10d
Jose Ortiz <jortiz@mememo.com>
2019-08-20 22:19:28 -0400

    add login view 60%

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,8 +3,15 @@
     <notification-component :label="label" />
     <div class="content-component">
       <div class="content">
-        <logo-component :source="'logo_1.png'" :width="'120px'" :height="'120px'" :is-visible="isVisible" />
-        <label>Iniciar Sesion</label>
+        <logo-component :source="'logo_1.png'" :width="'140px'" :height="'140px'" :is-visible="isVisible" />
+        <LabelComponent :fontSize="'40'" :color="'#DAB822'" :label="login" />
+        <form ref="form" class="form-control-lg">
+          <input v-model="username" type="email" :placeholder="email" class="input-type">
+          <input v-model="password" type="password" :placeholder="pass" class="input-type">
+          <b-button variant="primary border-0">Button</b-button>
+
+        </form>
+
       </div>
     </div>
   </div>
@@ -13,18 +20,25 @@
 <script>
 import Strings from '@/components/strings.js'
 import LogoComponent from '@/components/LogoComponent.vue'
+import LabelComponent from '@/components/LabelComponent.vue'
 import NotificationComponent from '@/components/NotificationComponent.vue'
 export default {
   name: 'login',
   data () {
     return {
+      username: '',
+      password: '',
+      isVisible: false,
       label: Strings.login,
-      isVisible: false
+      login: Strings.loginLabel,
+      email: Strings.email,
+      pass: Strings.password
     }
   },
   components: {
     Strings,
     LogoComponent,
+    LabelComponent,
     NotificationComponent
   }
 }
